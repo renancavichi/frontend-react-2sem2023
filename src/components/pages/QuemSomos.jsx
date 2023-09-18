a6db63e2f13db0ece157446c63914e5ec0f5a046
Renan Cavichi <renancavichi@gmail.com>
2023-09-18 18:01:48 -0300

    create card user

--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -2,7 +2,20 @@ import Header from "../layout/Header"
 import Sidebar from "../layout/Sidebar"
 import Content from "../layout/Content"
 import Footer from "../layout/Footer"
+import CardUser from "../ui/CardUser"
 
+const users = [
+{
+  nome: "Renan Cavichi",
+  email: "renancavichi@gmail.com",
+  photo: "https://avatars.githubusercontent.com/u/4259630?v=4"
+},
+{
+  nome: "Maria",
+  email: "maria@gmail.com",
+  photo: "https://avatars.githubusercontent.com/u/141092604?v=4"
+}
+] 
 const QuemSomos = () => {
   return (
     <>
@@ -11,6 +24,11 @@ const QuemSomos = () => {
             <Sidebar />
             <Content>
               <h1>Quem Somos</h1>
+              {
+                users.map((user) => {
+                  return <CardUser user={user} />
+                })
+              }
             </Content>
         </div>
         <Footer />
---
+++ src/components/ui/CardUser.jsx
@@ -0,0 +1,15 @@
+import './CardUser.css'
+
+const CardUser = ({user}) => {
+  return (
+    <div className="card-user">
+        <img src={user.photo} alt={user.nome} />
+        <div>
+            <h3>{user.nome}</h3>
+            <span>{user.email}</span>
+        </div>
+    </div>
+  )
+}
+
+export default CardUser
