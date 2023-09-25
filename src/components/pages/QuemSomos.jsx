a649aa03f96ef6144aa89123c6c573bf7ae16458
Renan Cavichi <renancavichi@gmail.com>
2023-09-25 17:47:09 -0300

    add useEffect to list user

--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -3,20 +3,28 @@ import Sidebar from "../layout/Sidebar"
 import Content from "../layout/Content"
 import Footer from "../layout/Footer"
 import CardUser from "../ui/CardUser"
+import { useEffect, useState } from "react"
 
-const users = [
-{
-  nome: "Renan Cavichi",
-  email: "renancavichi@gmail.com",
-  photo: "https://avatars.githubusercontent.com/u/4259630?v=4"
-},
-{
-  nome: "Maria",
-  email: "maria@gmail.com",
-  photo: "https://avatars.githubusercontent.com/u/141092604?v=4"
-}
-] 
+
 const QuemSomos = () => {
+
+    const [users, setUsers] = useState([])
+     
+
+    useEffect(()=>{
+      
+      const getUsers = async () => {
+        const response = await fetch('http://localhost:3300/user/list')
+        const data = await response.json()
+        console.log(data.success)
+        console.log(data.users)
+        setUsers(data.users)
+      }
+
+      getUsers()
+      
+    }, [])
+
   return (
     <>
         <Header />
@@ -25,9 +33,9 @@ const QuemSomos = () => {
             <Content>
               <h1>Quem Somos</h1>
               {
-                users.map((user) => {
-                  return <CardUser user={user} />
-                })
+                users.length > 0 ? users.map((user) => {
+                  return <CardUser key={user.id} user={user} />
+                }): <p>Carregando...</p>
               }
             </Content>
         </div>
--- src/components/ui/CardUser.jsx
+++ src/components/ui/CardUser.jsx
@@ -3,9 +3,9 @@ import './CardUser.css'
 const CardUser = ({user}) => {
   return (
     <div className="card-user">
-        <img src={user.photo} alt={user.nome} />
+        <img src={user.photo} alt={user.name} />
         <div>
-            <h3>{user.nome}</h3>
+            <h3>{user.name}</h3>
             <span>{user.email}</span>
         </div>
     </div>
